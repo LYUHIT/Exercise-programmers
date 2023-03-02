97ad79076edfe11865d3a320001e4b72fb6d7e1c
Uiseok YU <116103097+LYUHIT@users.noreply.github.com>
2023-03-02 11:54:07 +0900

    Update 햄버거 만들기.js

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -2,16 +2,16 @@ function solution(ingredient) {
     var answer = 0;
     let made = 0;
 
-        for (let i = 0 ; i <= ingredient.length - 4; i++) {
-            if (ingredient[i] == 1
-                && ingredient[i + 1] == 2
-                && ingredient[i + 2] == 3
-                && ingredient[i + 3] == 1) {
+    for (let i = 0 ; i <= ingredient.length - 4; i++) {
+        if (ingredient[i] == 1
+            && ingredient[i + 1] == 2
+            && ingredient[i + 2] == 3
+            && ingredient[i + 3] == 1) {
                 made++;
                 ingredient.splice(i, 4)
                 i -= 4;
-            }
         }
+    }
     answer = made;
     return answer;
 }
