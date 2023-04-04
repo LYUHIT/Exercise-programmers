57fe2b350af375652034bba8d85c7ecf23f145c7
Uiseok YU <116103097+LYUHIT@users.noreply.github.com>
2023-04-05 02:13:36 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -5,11 +5,16 @@ HAVING FAVORITES = (
   SELECT MAX(FAVORITES)
   FROM REST_INFO AS R2
   WHERE R2.FOOD_TYPE = R1.FOOD_TYPE
+    
     # R1 : REST_INFO에서 (FOOD_TYPE,FAVORITES) 로 그룹화 된 상태
     # R2 : REST_INFO 본연의 상태
-    # 따라서, R2.FOOD_TYPE = R1.FOOD_TYPE 조건은
-    # 현재 R1에서의 튜플이 가진 FOOD_TYPE이, R2에서 FOOD_TYPE 과 일치하는 R2 튜플을 모은다.
-    # 그 튜플들의 MAX(FAVORITES)을 계산하고, 
-    # HAVING절을 통해 이 그룹에서 그 값과 같은 것만 남긴다.
+    
+    # 서브쿼리의 진행 과정은 다음과 같다.
+    
+    # 'WHERE R2.FOOD_TYPE = R1.FOOD_TYPE': 현재 R1에서의 튜플이 가진 FOOD_TYPE이 R2에서 FOOD_TYPE 과 일치하는 R2 튜플을 모은다.
+    
+    # SELECT MAX(FAVORITES) : 그 튜플들의 MAX(FAVORITES)을 계산하고, 
+    
+    # HAVING FAVORITES = (...) : HAVING절을 통해 이 그룹에서 그 값과 같은 것만 남긴다.(조건문역할)
 )
 ORDER BY FOOD_TYPE DESC;
