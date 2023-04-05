be43692f0eaa471070c288cc3dc4709ede319fdf
Uiseok YU <116103097+LYUHIT@users.noreply.github.com>
2023-04-05 13:22:30 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,17 +1,18 @@
 def solution(n, computers):
     answer = 0
+    
+    queue = []
     visited = []
     
-    def newRoot (i,n,visited):
-        for j in range(n):
-            if computers[i][j] == 1 and j not in visited:
-                visited.append(j)
-                newRoot(j,n,visited)
-            
-    for i in range(n):
-        if i not in visited :
-            visited.append(i)
-            answer = answer+1
-            newRoot(i,n,visited)
-
+    for a in range(n):
+        if a not in visited:
+            queue.append(a)
+            answer += 1
+            while queue :
+                now = queue.pop(0)    
+                for i in range(n):
+                    if computers[now][i] == 1 :
+                        if i not in visited:
+                            visited.append(i)
+                            queue.append(i)
     return answer
