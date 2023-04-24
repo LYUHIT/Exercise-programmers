b4f48fd85557f0b8f8c7640cbbeb8da4fa1a8109
Uiseok YU <116103097+LYUHIT@users.noreply.github.com>
2023-04-24 18:08:17 +0900

    [level 3] Title: 기지국 설치, Time: 5.27 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
@@ -3,27 +3,25 @@ import math
 def solution(n, stations, w):
     answer = 0
     possible = []
-    
+
     # 통신 가능한 범위 정리
     for station in stations :
         if len(possible) != 0 and station-w <= possible[-1][1]:
             possible[-1][1] = station+w
         else :
             possible.append([station-w,station+w])
-    
+
     def countBase(i):
         share = i//(w*2+1)
         remain = i%(w*2+1)
-        if remain > 0 :
-            return share+1
-        else:
-            return share
-    
+        if remain > 0 : return share+1
+        else : return share
+
     # 시작점 ~ 첫 통신범위 탐색
     if possible[0][0] > 1:
         impossible = possible[0][0]-1
         answer += countBase(impossible)
-    
+
     # 첫 통신범위 ~ 끝까지 필요한 기지국 수 탐색     
     for i in range(len(possible)):
         if i == len(possible)-1:
@@ -31,5 +29,5 @@ def solution(n, stations, w):
         else:
             impossible = possible[i+1][0]-possible[i][1]-1
         answer += countBase(impossible)
-        
+
     return answer
