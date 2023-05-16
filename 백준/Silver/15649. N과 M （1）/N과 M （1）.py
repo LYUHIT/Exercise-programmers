1e1e0df010ea5a1da059439402cb016c90541530
lyuhit <116103097+LYUHIT@users.noreply.github.com>
2023-05-16 11:30:45 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -2,24 +2,23 @@ import sys
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
+visited = [0] * n
+nums = [0] * m
 answer = []
 
-def dfs(num, visited):
-    if len(num) == m:
-        result = " ".join(num)
-        answer.append(result)
+
+def dfs(k):
+    if k == m:
+        print(" ".join(nums))
     else:
         for i in range(n):
-            tnum = num.copy()
-            tvisited = visited
-            if visited & (1 << i):
+            if visited[i]:
                 continue
             else:
-                tvisited |= 1 << i
-                tnum.append(str(i + 1))
-                dfs(tnum, tvisited)
-    return
+                visited[i] = 1
+                nums[k] = str(i + 1)
+                dfs(k + 1)
+                visited[i] = 0
 
-dfs([], 0)
 
-print("\n".join(answer))
+dfs(0)
