53c4eb93dbf156de0f57512e44cea1dd12f09dc9
UISEOK <116103097+LYUHIT@users.noreply.github.com>
2023-05-30 12:17:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 648 ms, Memory: 137184 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,24 +1,18 @@
 import sys
 
 input = sys.stdin.readline
-
 n = int(input().strip())
 cards = list(map(int, input().split()))
 m = int(input().strip())
-test = list(map(int, input().split()))
+tests = list(map(int, input().split()))
+
 answer = []
 cartDict = dict()
 
 for card in cards:
-    if cartDict.get(card):
-        cartDict[card] += 1
-    else:
-        cartDict[card] = 1
+    cartDict[card] = cartDict.get(card, 0) + 1
 
-for t in test:
-    if cartDict.get(t):
-        answer.append(str(cartDict[t]))
-    else:
-        answer.append("0")
+for test in tests:
+    answer.append(str(cartDict.get(test, 0)))
 
 print(" ".join(answer))
