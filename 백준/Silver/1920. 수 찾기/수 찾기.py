2249dfe5ace7e35a6e8a1ac6d266c8ebbb6eb3eb
UISEOK <116103097+LYUHIT@users.noreply.github.com>
2023-05-30 11:55:20 +0900

    [Silver IV] Title: 수 찾기, Time: 472 ms, Memory: 47268 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -7,27 +7,13 @@ m = int(input().strip())
 b = list(map(int, input().split()))
 
 a.sort()
-l = len(a)
 
 def find(s, e, f):
     m = (e + s) // 2
-
-    if s == e:
-        if l <= m:
-            return 0
-        elif a[m] == f:
-            return 1
-        else:
-            return 0
-    else:
-        if a[m] == f:
-            return 1
-        else:
-            if a[m] < f:
-                return find(m + 1, e, f)
-            else:
-                return find(s, m, f)
-
+    if s == e:         return 0
+    if a[m] == f:      return 1
+    elif a[m] < f:     return find(m + 1, e, f)
+    else:              return find(s, m, f)
 
 for j in b:
-    print(find(0, l, j))
+    print(find(0, len(a), j))
